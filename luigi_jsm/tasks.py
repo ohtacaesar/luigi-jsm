271e72c800565b1cf0ce00162d99a14320e29b61
Takahiro Ohtomo <ohtacaesar@gmail.com>
2016-05-28 12:52:01 +0900

    日付ごとに実行するように

--- luigi_jsm/tasks.py
+++ luigi_jsm/tasks.py
@@ -3,15 +3,21 @@
 import jsm
 import luigi
 import pandas as pd
+import os
 
 q = jsm.Quotes()
 
 
 class ExtractBrandTask(luigi.Task):
+    date = luigi.DateParameter()
+
     def output(self):
-        return luigi.LocalTarget('data/brands.csv')
+        return luigi.LocalTarget('data/%s/brands.csv' % str(self.date))
 
     def run(self):
+        # ディレクトリ作成
+        os.makedirs(os.path.dirname(self.output().path))
+
         # '5250' # 情報・通信 
         brands = q.get_brand('5250')
         df = pd.DataFrame([[b.ccode, b.market, b.name, b.info] for b in brands])
